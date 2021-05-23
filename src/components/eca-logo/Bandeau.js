931a15ebba61b7b5c0e4bae23b344a4eba1eebd1
MGeraud <geraud.mon@gmail.com>
2021-05-23 11:44:56 +0200

    Create ECA Logo component

---
+++ src/components/eca-logo/Bandeau.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Bandeau.css";
+import logoescapade from "./escapadelogo.jpg";
+
+const Bandeau = () => {
+    return(
+        <div className="bandeau__background">
+            <img src={logoescapade} alt="defaut" />
+        </div>
+    )
+}
+
+export default Bandeau;
