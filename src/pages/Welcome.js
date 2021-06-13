e173b6078a9df5441229e3a51f887e026cb5da63
MGeraud <geraud.mon@gmail.com>
2021-06-13 16:29:16 +0200

    Add some css to welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,24 +1,27 @@
 import {Link} from "react-router-dom";
+import classNames from './Welcome.module.css';
+import {Fragment} from "react";
 
 const Welcome = () => {
     return (
-        <>
-            <h1>coucou</h1>
-            <ul>
-                <li>
-                    <Link to='/resultats'>Results</Link>
-                </li>
-                <li>
-                    <Link to='/juges'>Juges</Link>
-                </li>
-                <li>
-                    <Link to='/inscriptions'>Inscritpions</Link>
-                </li>
-                <li>
-                    <Link to='/gestion'>Gestion</Link>
-                </li>
-            </ul>
-        </>
+        <Fragment className={classNames.displayLinks}>
+            <Link to='/resultats'>
+                <button type='button'>Résultats</button>
+            </Link>
+
+            <Link to='/juges'>
+                <button type='button'>>Juges</button>
+            </Link>
+
+            <Link to='/inscriptions'>
+                <button type='button'>Inscritpions</button>
+            </Link>
+
+            <Link to='/gestion'>
+                <button type='button'>Gestion</button>
+            </Link>
+
+        </Fragment>
     )
 }
 
