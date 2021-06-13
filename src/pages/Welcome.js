c923d3034b1a8b8b1cdc1d0f21c50bddf60f4b26
MGeraud <geraud.mon@gmail.com>
2021-06-13 13:05:14 +0200

    Add Link to routes on Welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,24 @@
+import {Link} from "react-router-dom";
 
 const Welcome = () => {
-    return(
-        <h1>coucou</h1>
+    return (
+        <>
+            <h1>coucou</h1>
+            <ul>
+                <li>
+                    <Link to='/resultats'>Results</Link>
+                </li>
+                <li>
+                    <Link to='/juges'>Juges</Link>
+                </li>
+                <li>
+                    <Link to='/inscriptions'>Inscritpions</Link>
+                </li>
+                <li>
+                    <Link to='/gestion'>Gestion</Link>
+                </li>
+            </ul>
+        </>
     )
 }
 
