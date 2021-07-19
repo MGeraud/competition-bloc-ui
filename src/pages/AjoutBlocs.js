042ad0be68147e13def043da4f458d392dddcaef
MGeraud <geraud.mon@gmail.com>
2021-07-19 15:08:50 +0200

    Add form to AjoutBlocs.js

--- src/pages/AjoutBlocs.js
+++ src/pages/AjoutBlocs.js
@@ -1,8 +1,26 @@
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {fetchCategories} from "../store/categoriesSlice";
+import axios from "axios";
+import {ErrorMessage, Field, FieldArray, Form, Formik} from "formik";
+import * as Yup from "yup";
+import TextError from "../components/forms/TextError";
 
+
+const initialValues = {
+    id: '',
+    boulders: [''],
+}
+
+
+const axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json',
+    }
+}
 const AjoutBlocs = () => {
+    const [succes, setSucces] = useState();
+    const [failure, setFailure] = useState();
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -18,9 +36,83 @@ const AjoutBlocs = () => {
             {loadingState === 'fulfilled' &&
             <ul>
                 {categories.map((categorie) => (
-                    <li>{categorie.categoryName}</li>
+                    <li>{categorie.categoryName}
+                        <ul>{categorie.boulders.map((bloc)=>
+                            <li>{bloc}</li>)}
+                        </ul>
+                    </li>
                 ))}
             </ul>}
+
+            {loadingState === 'fulfilled' &&
+            <Formik
+                initialValues={initialValues}
+
+                // envoi des données du formulaire vers l'api backend
+                onSubmit={
+                    async (values, onSubmitProps) => {
+
+                        await axios.post('http://localhost:8081/competition/add', values, axiosConfig)
+                            .then(response => {
+                                setSucces(true);
+                                setFailure(false);
+                            })
+                            .catch(e => {
+                                setFailure(true);
+                                setSucces(false);
+                            })
+                            .then(onSubmitProps.resetForm)
+
+                    }}
+            >
+                {formik => {
+                    return (
+                        <Form>
+                            <div>
+                                <label htmlFor='id'></label>
+                                <Field as='select' id='id' name='id'>
+                                    {categories.map((categorie) => {
+                                        return (
+                                            <option key={categorie.id}
+                                                    value={categorie.id}>{categorie.categoryName}</option>
+                                        )
+                                    })}
+                                </Field>
+                                <ErrorMessage name='id' component={TextError}/>
+                            </div>
+                            <div>
+                                <label>Liste de bloc à ajouter</label>
+                                {/*utilisation de FieldArray de formik pour créer directement un array de categories*/}
+                                <FieldArray name='boulders'>
+                                    {fieldArrayProps => {
+                                        const {push, remove, form} = fieldArrayProps;
+                                        const {values} = form;
+                                        const {boulders} = values;
+                                        return (
+                                            <div>
+                                                {boulders.map((boulder, index) => (
+                                                    <div key={index}>
+                                                        <Field name={`boulders[${index}]`}/>
+                                                        {index > 0 && (
+                                                            <button type='button'
+                                                                    onClick={() => remove(index)}>-</button>
+                                                        )}
+                                                        <button type='button' onClick={() => push('')}>+</button>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        )
+                                    }}
+                                </FieldArray>
+                            </div>
+                            <button type='submit' disabled={!formik.isValid || formik.isSubmitting}>Envoyer</button>
+                        </Form>
+                    )
+                }}
+
+            </Formik>}
+
+
         </>
     )
 }
